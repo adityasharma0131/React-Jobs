6cdd738005097118a2bf83d641b193a657164853
adityasharma0131 <adityasharma0431@gmail.com>
2024-05-22 14:39:31 +0530

    loader, single JobpAge and json server connection with linking

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage'
+import JobPage from './pages/JobPage';  
 import NotFoundPage from './pages/NotFoundPage';
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/job/:id" element={<JobPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
