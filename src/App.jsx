b7bd28c031160ba279ec29e0568aed72413d37b7
adityasharma0131 <adityasharma0431@gmail.com>
2024-05-24 00:23:23 +0530

    ediJob with toast

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,27 @@ const App = () => {
     }
   };
 
+  const updateJob = async (job) =>{
+    try {
+      const res = await fetch(`/api/jobs/${job.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to edit job");
+      }
+      const data = await res.json();
+      toast.success("Job Edited successfully!");
+    } catch (error) {
+      toast.error("Error edited job: " + error.message);
+      console.error("Error edited job:", error);
+    }
+  }
+
+
   const deleteJob = async (id) => {
     try {
       const res = await fetch(`/api/jobs/${id}`, {
@@ -56,8 +77,8 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/job/:id" element={<JobPage deleteJob={deleteJob} />} />
-        <Route path="/edit-job/:id" element={<EditJobPage  />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
